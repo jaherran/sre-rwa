1a170de2ff28c22492f5af6789044aa48d28cb68
Jorge Herran <jorge.herran@endava.com>
2021-01-04 17:26:04 -0500

    Final script files

--- task1/script.sh
+++ task1/script.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 echo "Publishing an index.html in AWS"
-
-echo "Checking if AWS ENV variables are set"
-[ -z "$AWS_ACCESS_KEY_ID" ] && echo "Need to set AWS_ACCESS_KEY_ID" && exit 1;
-[ -z "$AWS_SECRET_ACCESS_KEY" ] && echo "Need to set AWS_SECRET_ACCESS_KEY" && exit 1;
-[ -z "$AWS_DEFAULT_REGION" ] && echo "Need to set AWS_DEFAULT_REGION" && exit 1;
-
-echo "AWS_ACCESS_KEY_ID: $AWS_ACCESS_KEY_ID"
-echo "AWS_SECRET_ACCESS_KEY: Not showing you that!"
-echo "AWS_DEFAULT_REGION: $AWS_DEFAULT_REGION"
 echo "Checking if AWS CLI is installed"
-command -v aws >/dev/null 2>&1 || { echo >&2 "I require aws but it's not installed.  Aborting."; exit 1; }
-echo "IAM Get User"
-aws iam get-user
+command -v aws >/dev/null 2>&1 || { echo >&2 "I require aws but it's not installed.  Please install it Aborting."; exit 1; }
 
-echo "Creating the S3 bucket"
+echo "Creating your aws profile"
+aws configure --profile task1
+#read -p 'Provide your AWS Access Key  : ' AWS_ACCESS_KEY_ID
+#read -p 'Provide your AWS Secret : ' AWS_SECRET_ACCESS_KEY
+#read -p 'Provide your AWS Region: ' AWS_DEFAULT_REGION
+export AWS_PROFILE=task1
 
-echo "Copying the website into the bucket"
+echo "creating the keypair to use in the instance, store locally and modify its permissions"
+aws ec2 create-key-pair --key-name task1 --query 'KeyMaterial' --output text > task1.pem
+chmod 600 task1.pem
 
-#aws s3 cp build s3://<Your bucket name>/<Your folder name> --acl public-read --recursive
+echo "Execute the cloudformation script"
+aws cloudformation create-stack --stack-name task1 --template-body file://deployindex.yml --parameters ParameterKey=KeyName,ParameterValue=task1 ParameterKey=SSHLocation,ParameterValue=0.0.0.0/0> /dev/null 2>&1
+
+echo "Testing the site going to: "
+aws cloudformation describe-stacks --stack-name task1 --query "Stacks[0].Outputs[?OutputKey=='PublicIP'].OutputValue" --output text>PublicIP
